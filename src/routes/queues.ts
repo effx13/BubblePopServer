cf70bdedd48c499f4c1f4b0dddfcee3b1836299f
eFFx <chanoo104@naver.com>
2022-03-21 18:54:54 +0900

    feat: Get Queue Count

--- src/routes/queues.ts
+++ src/routes/queues.ts
@@ -3,13 +3,25 @@ import express from 'express';
 
 const router = express.Router();
 
-// Find All and Sort By QueuedAt
+// Find All and Sort By Reservation
 router.get('/', (req, res) => {
   Queue.find()
-    .sort({ queuedAt: 1 })
+    .sort({ reservation: 1 })
     .then((queue: string | any[]) => {
-      if (!queue.length) return res.status(404).send({ e: 'Queue not found' });
+      if (!queue.length) return res.status(404).send({ Error: 'Queue not found' });
       res.send(queue);
+    })
+    .catch((e: any) => res.status(500).send(e));
+});
+
+// Get Queue Count
+router.get('/count', (req, res) => {
+  Queue.find()
+    .sort({ reservation: 1 })
+    .then((queue: string | any[]) => {
+      res.send({
+        count: queue.length,
+      });
     })
     .catch((e: any) => res.status(500).send(e));
 });
@@ -18,7 +30,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   Queue.create(req.body)
     .then((queue) => res.send(queue))
-    .catch((err) => res.status(500).send(err));
+    .catch((e) => res.status(500).send(e));
 });
 
 export default router;
